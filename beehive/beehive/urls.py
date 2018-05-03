863806d5aef14b2f5285d35920632fb0b6eabd86
pakam2 <pkami.87@gmail.com>
2018-05-03 22:14:11 +0200

    Added main view for the beehive site

--- beehive/beehive/urls.py
+++ beehive/beehive/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from hives.views import MainView
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', MainView.as_view(), name="main"),
 ]
