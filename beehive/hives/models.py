93cbf06375beb4b94e944257cb25ff41248f0074
pakam2 <pkami.87@gmail.com>
2018-05-04 21:58:18 +0200

    Added hive model

--- beehive/hives/models.py
+++ beehive/hives/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
+
+class HiveModel(models.Model):
+    numberOfHive = models.IntegerField(null=False)
+    firstFrame = models.DecimalFiled(max_digits=3, decimal_places=2, null=False)
+    secondFrame = models.DecimalField(max_digits=3, decimal_places=2, null=False)
+    thirdFrame = models.DecimalField(max_digits=3, decimal_places=2, null=False)
+    motherBee = models.BooleanField(default=False, null=False)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    comments = models.CharField(max_length=500)
